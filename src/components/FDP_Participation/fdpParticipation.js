fef8ed0c4cffd04cc80d3fe6b02cd56685ae2c1c
Pulkit Dwivedi <pulkitdwivedi123@gmail.com>
2024-08-01 00:16:07 +0530

    fix: fdp-part xlxc responsivity

--- src/components/FDP_Participation/fdpParticipation.js
+++ src/components/FDP_Participation/fdpParticipation.js
@@ -8,28 +8,30 @@ class FdpParticipationPage extends Component {
   render() {
     return (
       <>
-        {" "}
-        <Container className="global-container text-center" fluid>
-          <Row>
-            <Col>
-              <h3>FACULTY DEVELOPMENT PROGRAMME (FDP) ATTENDED</h3>
-            </Col>
-          </Row>
-        </Container>
-        <div className="iframe-container">
+        <div style={{height: "1305vh"}}>
           {" "}
-            {/* <iframe
-              scrollbar="no"
-              src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRmib8LOBnFRGL16HLnpYgtAdBa2VMKzBS5NnkDU1kWQXqzmL5jlit7kS8hbjLF3g/pubhtml?widget=true&amp;headers=false"
-              width="100%"
-              height="800px"
-              style={{ border: "0" }}
-              title="Spreadsheet"
-            /> */}
-            <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSK28cg66PabKJCoaTK1Wv8SQS9xnQIdpFHW-3DS3Wzyu27KpriYGd4QLIPyj2yBw/pubhtml?gid=427480530&amp;single=true&amp;widget=true&amp;headers=false" title="Spreadsheet" width="100%"
-              height="100%"
-              style={{ border: "0", height: "100%", scale: "1.25" }}>
-            </iframe>
+            <Container className="global-container text-center" fluid>
+              <Row>
+                <Col>
+                  <h3>FACULTY DEVELOPMENT PROGRAMME (FDP) ATTENDED</h3>
+                </Col>
+              </Row>
+            </Container>
+            <div className="iframe-container">
+              {" "}
+                {/* <iframe
+                  scrollbar="no"
+                  src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRmib8LOBnFRGL16HLnpYgtAdBa2VMKzBS5NnkDU1kWQXqzmL5jlit7kS8hbjLF3g/pubhtml?widget=true&amp;headers=false"
+                  width="100%"
+                  height="800px"
+                  style={{ border: "0" }}
+                  title="Spreadsheet"
+                /> */}
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSK28cg66PabKJCoaTK1Wv8SQS9xnQIdpFHW-3DS3Wzyu27KpriYGd4QLIPyj2yBw/pubhtml?gid=427480530&amp;single=true&amp;widget=false&amp;headers=false&amp;rm=minimal&amp;chrome=false&amp;range=A1:L163"
+                  title="Spreadsheet"
+                  style={{ border: "1", height: "100%", width: "73.1%"}}>
+                </iframe>
+            </div>
         </div>
       </>
     );
