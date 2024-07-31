8e9fe5781dba516d61b2040aee531879594c1ddf
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-07-31 14:06:48 +0530

    chore: manually build gh-pages

--- src/container/gallery/Lab507Data.js
+++
@@ -1,4 +0,0 @@
-export const IMAGES = [
-  { src: "https://i.ibb.co/McJJTCN/image.jpg", thumbnail: "https://i.ibb.co/McJJTCN/image.jpg" },
-{ src: "https://i.ibb.co/mTkbMBQ/1.jpg", thumbnail: "https://i.ibb.co/mTkbMBQ/1.jpg" },
-];
--- src/container/gallery/LibraryPhotoData.js
+++
@@ -1,5 +0,0 @@
-export const IMAGES = [
-  { src: "https://i.ibb.co/S3qtZg9/111.jpg", thumbnail: "" },
-  { src: "https://i.ibb.co/S3qtZg9/112.jpg", thumbnail: "" },
-  
-];
