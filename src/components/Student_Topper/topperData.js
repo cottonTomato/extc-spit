bcd1978d539777a6f57cfeb705ca7bcbacff75f9
Pulkit Dwivedi <pulkitdwivedi123@gmail.com>
2024-11-02 10:14:41 +0530

    added topper data

--- src/components/Student_Topper/topperData.js
+++ src/components/Student_Topper/topperData.js
@@ -1,6 +1,24 @@
 const toppers = [
   {
     category:
+      "B.Tech Electronics &Telecommunication Engineering Sem VIII MAY 2024",
+    students: [
+      { rank: 1, name: "Gawas Narayan Dhayaba", cgpi: 9.79 },
+      { rank: 2, name: "Aditya Charudatta Kulkarni", cgpi: 9.67 },
+      { rank: 3, name: "Nishali Dhanshika Gowri Venkadeshaaral", cgpi: 9.59 },
+    ],
+  },
+  {
+    category:
+      "B.Tech Electronics Engineering Sem VIII MAY 2024",
+    students: [
+      { rank: 1, name: "Dedhia Fenil Bharat", cgpi: 9.21 },
+      { rank: 2, name: "Jogi Harshita Umesh", cgpi: 9.04 },
+      { rank: 3, name: "Surve Bhavesh Sadanand", cgpi: 9.03 },
+    ],
+  },
+  {
+    category:
       "B.Tech Electronics &Telecommunication Engineering Sem VIII MAY 2023",
     students: [
       { rank: 1, name: "Agrawal Khushi Kirtesh", cgpi: 9.81 },
@@ -17,15 +35,6 @@ const toppers = [
     ],
   },
   {
-    category:
-      "B.Tech Electronics &Telecommunication Engineering Sem II MAY 2023",
-    students: [
-      { rank: 1, name: "Dhamne Mansi Abhijit", cgpi: 9.74 },
-      { rank: 2, name: "Jhalani Manavi Nishant", cgpi: 9.67 },
-      { rank: 3, name: "Shukla Ruchir Surendra", cgpi: 9.36 },
-    ],
-  },
-  {
     category: "B.Tech Electronics &Telecommunication Sem VIII MAY 2022",
     students: [
       { rank: 1, name: "Chaluvadi LV Aditi Umadevi", cgpi: 9.96 },
@@ -39,22 +48,6 @@ const toppers = [
       { rank: 1, name: "Naik Isha Deepak Jayshree", cgpi: 9.91 },
       { rank: 2, name: "Vasant Nisha Nitesh Jigna", cgpi: 9.89 },
       { rank: 3, name: "Sanghvi Kunal Sanjay Julie", cgpi: 9.88 },
-    ],
-  },
-  {
-    category: "B.Tech Electronics &Telecommunication Sem VI MAY 2022",
-    students: [
-      { rank: 1, name: "Agrawal Khushi Kritesh Manisha", cgpi: 9.77 },
-      { rank: 2, name: "Gupta Garima Divesh", cgpi: 9.71 },
-      { rank: 3, name: "Raghuwanshi Ishika Rajesh", cgpi: 9.7 },
-    ],
-  },
-  {
-    category: "B.Tech Electronics Engineering Sem VI MAY 2022",
-    students: [
-      { rank: 1, name: "Dave Jenil Mayank", cgpi: 9.86 },
-      { rank: 2, name: "Asodekar Eeshaan Paresh", cgpi: 9.79 },
-      { rank: 3, name: "Sutar Pushkar Ramesh", cgpi: 9.67 },
     ],
   },
 ];
