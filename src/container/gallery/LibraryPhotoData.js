8433ac3dfce9ab35b7612861c4da23d92fb79209
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2024-11-15 16:21:38 +0530

    update: dept lib

--- src/container/gallery/LibraryPhotoData.js
+++ src/container/gallery/LibraryPhotoData.js
@@ -1,5 +1,8 @@
 export const IMAGES = [
   { src: "https://i.ibb.co/S3qtZg9/111.jpg", thumbnail: "" },
   { src: "https://i.ibb.co/S3qtZg9/112.jpg", thumbnail: "" },
-  
+  { src: "https://i.ibb.co/fp0FhNK/1731390309406.jpg", thumbnail: "" },
+  { src: "https://i.ibb.co/kBTjWCB/1731390309485.jpg", thumbnail: "" },
+  { src: "https://i.ibb.co/2cTz27Q/1731390309552.jpg", thumbnail: "" },
 ];
+
